4381ab6fd515f519f9c0e73082897f8fc3216c51
Sam Ulrich <samson98ulrich@gmail.com>
2019-08-25 18:11:25 -0500

    Corrected addressForEntity Buffer Gap

--- solidity_util.go
+++ solidity_util.go
@@ -1,6 +1,8 @@
 package hedera
 
 import (
+	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"errors"
 )
@@ -10,30 +12,37 @@ const (
 	ADDRESS_LEN_HEX = ADDRESS_LEN * 2
 )
 
-func addressForEntity(shard int64, realm int64, entity int64) string {
+func addressForEntity(shard int64, realm int64, entity int64) (string, error) {
 	// TODO: Add check to ensure 'shard' is within 32 bit range
-	var buf []byte
-	buf = append(buf, byte(shard))
-	buf = append(buf, byte(realm))
-	buf = append(buf, byte(entity))
-	return hex.EncodeToString(buf)
+	buf := new(bytes.Buffer)
+
+	err := binary.Write(buf, binary.BigEndian, int32(shard))
+	if err != nil { return "", err }
+
+	err = binary.Write(buf, binary.BigEndian, realm)
+	if err != nil { return "", err }
+
+	err = binary.Write(buf, binary.BigEndian, entity)
+	if err != nil { return "", err }
+
+	return hex.EncodeToString(buf.Bytes()), nil
 }
 
-func addressForAccount(accountID AccountID) string {
+func AddressForAccount(accountID AccountID) (string, error) {
 	return addressForEntity(
 		accountID.Shard,
 		accountID.Realm,
 		accountID.Account)
 }
 
-func addressForContract(contractID ContractID) string {
+func AddressForContract(contractID ContractID) (string, error) {
 	return addressForEntity(
 		contractID.Shard,
 		contractID.Realm,
 		contractID.Contract)
 }
 
-func addressForFile(fileID FileID) string {
+func AddressForFile(fileID FileID) (string, error) {
 	return addressForEntity(
 		fileID.Shard,
 		fileID.Realm,
